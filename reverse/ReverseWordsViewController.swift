df4385235a1716f3360f0f71c66f9e3c3a9cb9f2
Oleksandr <sashamelnik2306@gmail.com>
2022-07-06 16:50:46 +0300

    revision 2.1

---
+++ reverse/ReverseWordsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReverseWordsViewController.swift
+//  reverse
+//
+//  Created by Oleksandr Melnyk on 06.07.2022.
+//
+
+import UIKit
+
+class ReverseWordsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
